6cbf43e05caf8b7f2ac681d986a88680c7e034f2
Erich <erichnguyen25@gmail.com>
2023-11-29 13:20:25 -0600

    POST endpoint for Creating a Spot

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,14 +1,11 @@
 const router = require('express').Router();
-const sessionRouter = require('./session.js');
-const usersRouter = require('./users.js');
-const spotsRouter = require('./spots.js');
 const { restoreUser } = require('../../utils/auth.js');
 
 router.use(restoreUser);
 
-router.use('/session', sessionRouter);
-router.use('/users', usersRouter);
-router.use('/spots', spotsRouter);
+router.use('/session', require('./session.js'));
+router.use('/users', require('./users.js'));
+router.use('/spots', require('./spots.js'));
 
 router.post('/test', (req, res) => {
 	res.json({ requestBody: req.body });
--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -1,7 +1,43 @@
 const express = require('express');
 const router = express.Router();
 const { User, Spot, SpotImage } = require('../../db/models');
+const { check } = require('express-validator');
+const { handleValidationErrors } = require('../../utils/validation');
 const { requireAuth } = require('../../utils/auth');
+
+const spotNotFound = `Spot couldn't be found`
+
+const validateSpotCreate = [
+  check('address')
+    .exists({ checkFalsy: true }).withMessage('Street address is required')
+    .isString().withMessage('body.address must be a string'),
+  check('city')
+    .exists({ checkFalsy: true }).withMessage('City is required')
+    .isString().withMessage('body.city must be a string'),
+  check('state')
+    .exists({ checkFalsy: true }).withMessage('State is required')
+    .isString().withMessage('body.state must be a string'),
+  check('country')
+    .exists({ checkFalsy: true }).withMessage('Country is required')
+    .isString().withMessage('body.country must be a string'),
+  check('lat')
+    .exists({ checkFalsy: true }).withMessage('Latitude is not valid')
+    .not().isString().withMessage('body.lat must be a number'),
+  check('lng')
+    .exists({ checkFalsy: true }).withMessage('Longitude is not valid')
+    .not().isString().withMessage('body.lng must be a number'),
+  check('name')
+    .exists({ checkFalsy: true })
+    .isLength({max: 50}).withMessage('Name must be less than 50 characters')
+    .isString().withMessage('body.name must be a string'),
+  check('description')
+    .exists({ checkFalsy: true }).withMessage('Description is required')
+    .isString().withMessage('body.description must be a string'),
+  check('price')
+    .exists({ checkFalsy: true }).withMessage('Price per day is require')
+    .not().isString().withMessage('body.price must be a number'),
+  handleValidationErrors
+];
 
 router.get('/', async (req,res) => {
   res.json({
@@ -10,8 +46,8 @@ router.get('/', async (req,res) => {
 })
 
 router.get('/current', requireAuth, async (req,res) => {
-	const { user } = req;
-
+	const { user } = req
+  
   res.json({
     Spots: await Spot.findAll({
       where: { ownerId: user.id }
@@ -28,7 +64,15 @@ router.get('/:spotId', async (req,res) => {
     ]
   })
   if(!specificSpot) res.status(404)
-  res.json(specificSpot || {message: "Spot couldn't be found"})
+  res.json(specificSpot || {message: spotNotFound})
+})
+
+router.post('/', requireAuth, validateSpotCreate, async (req,res) => {
+  const { user } = req
+  req.body.ownerId = user.id
+  const newSpot = await Spot.create(req.body)
+  
+  res.json(newSpot)
 })
 
 module.exports = router;
