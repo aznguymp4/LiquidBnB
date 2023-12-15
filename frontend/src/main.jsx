f06c367807c369c58d5128eb0590ed05046c3181
Erich <erichnguyen25@gmail.com>
2023-12-15 16:18:38 -0600

    Login and Modal

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import App from './App';
+import { Modal, ModalProvider } from './context/Modal';
 import './index.css';
 import { Provider } from 'react-redux';
-import { restoreCSRF, csrfFetch } from './store/csrf';
 import configureStore from './store';
+import { restoreCSRF, csrfFetch } from './store/csrf';
 import * as sessionActions from './store/session';
+
 const store = configureStore();
 
 if (import.meta.env.MODE !== 'production') {
@@ -17,9 +19,12 @@ if (import.meta.env.MODE !== 'production') {
 }
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-	<React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+  <React.StrictMode>
+    <ModalProvider>
+      <Provider store={store}>
+        <App/>
+        <Modal/>
+      </Provider>
+    </ModalProvider>
   </React.StrictMode>
 );
